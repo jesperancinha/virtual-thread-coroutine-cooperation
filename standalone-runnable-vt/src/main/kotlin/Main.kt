2867a1f4a48851f83d4d33daafc1c0ee391ab679
jesperancinha <jofisaes@gmail.com>
2024-08-31 22:57:01 +0200

    code cleanup

--- standalone-runnable-vt/src/main/kotlin/Main.kt
+++ standalone-runnable-vt/src/main/kotlin/Main.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.core.task.SimpleAsyncTaskExecutor
-import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.stereotype.Service
