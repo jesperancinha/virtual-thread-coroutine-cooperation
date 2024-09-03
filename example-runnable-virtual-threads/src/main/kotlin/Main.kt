c799570fd8a8f523e4585b5b8593b1b58a710adf
jesperancinha <jofisaes@gmail.com>
2024-09-03 22:23:30 +0200

    Uses retries in virtual threads example

--- example-runnable-virtual-threads/src/main/kotlin/Main.kt
+++ example-runnable-virtual-threads/src/main/kotlin/Main.kt
@@ -7,20 +7,41 @@ import java.util.*
 import kotlin.random.Random
 
 val logger: Logger = LoggerFactory.getLogger(User::class.java)
-
+val random = java.util.Random()
 data class User(val id: Long, val name: String, val email: String)
 
 fun sendMessage(text: String, users: List<User>) = users.chunked(50).map {
     Thread.startVirtualThread {
         it.map {
-            Thread.startVirtualThread { sendEmail(text, it) }
+            Thread.startVirtualThread {
+                retry(5, 500) {
+                    sendEmail(text, it)
+                }
+            }
         }.forEach { it.join() }
     }
+}
+
+fun <T> retry(maxRetries: Int, delayMillis: Long, block: () -> T): T {
+    var lastException: Exception? = null
+    repeat(maxRetries) {
+        try {
+            return block()
+        } catch (e: Exception) {
+            lastException = e
+            sleep(delayMillis)
+            logger.warn("Retry number: $it - Retrying ...")
+        }
+    }
+    throw lastException ?: IllegalStateException("Retry failed with no exception")
 }
 
 
 fun sendEmail(message: String, user: User) {
     sleep(1000)
+    random.nextBoolean().takeIf { it }
+        ?: random.nextBoolean().takeIf { it }
+        ?: throw RuntimeException()
     logger.info("sent to user $user")
 }
 
