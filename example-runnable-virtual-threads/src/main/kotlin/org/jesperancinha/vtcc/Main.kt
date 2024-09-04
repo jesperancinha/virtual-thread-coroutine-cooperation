70e309488ef9b03feb4556f0330249b6172dc699
jesperancinha <jofisaes@gmail.com>
2024-09-04 22:07:08 +0200

    Adds more examples

--- example-runnable-virtual-threads/src/main/kotlin/org/jesperancinha/vtcc/Main.kt
+++ example-runnable-virtual-threads/src/main/kotlin/org/jesperancinha/vtcc/Main.kt
@@ -1,24 +1,40 @@
 package org.jesperancinha.vtcc
 
+import org.jesperancinha.vtcc.MessageSender.Companion.logger
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.lang.Thread.sleep
+import java.lang.Thread.startVirtualThread
 import java.util.*
 import kotlin.random.Random
 
-val logger: Logger = LoggerFactory.getLogger(User::class.java)
-val random = java.util.Random()
 data class User(val id: Long, val name: String, val email: String)
 
-fun sendMessage(text: String, users: List<User>) = users.chunked(50).map {
-    Thread.startVirtualThread {
-        it.map {
-            Thread.startVirtualThread {
-                retry(5, 500) {
-                    sendEmail(text, it)
+class MessageSender {
+
+    fun sendMessage(text: String, users: List<User>) = users.chunked(50).map {
+        startVirtualThread {
+            it.map {
+                startVirtualThread {
+                    retry(5, 500) {
+                        sendEmail(text, it)
+                    }
                 }
-            }
-        }.forEach { it.join() }
+            }.forEach { it.join() }
+        }
+    }.forEach { it.join() }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(User::class.java)
+        val random = java.util.Random()
+
+        fun sendEmail(message: String, user: User) {
+            sleep(1000)
+            random.nextBoolean().takeIf { it }
+                ?: random.nextBoolean().takeIf { it }
+                ?: throw RuntimeException()
+            logger.info("sent message $message to user $user")
+        }
     }
 }
 
@@ -37,18 +53,11 @@ fun <T> retry(maxRetries: Int, delayMillis: Long, block: () -> T): T {
 }
 
 
-fun sendEmail(message: String, user: User) {
-    sleep(1000)
-    random.nextBoolean().takeIf { it }
-        ?: random.nextBoolean().takeIf { it }
-        ?: throw RuntimeException()
-    logger.info("sent to user $user")
-}
-
 fun main() {
-    sendMessage("This is a message", (1..100).map {
+    val messageSender = MessageSender()
+    messageSender.sendMessage("This is a message", (1..100).map {
         User(
             id = Random.nextLong(), name = "user-${UUID.randomUUID()}", email = "user-${UUID.randomUUID()}"
         )
-    }).forEach { it.join() }
+    })
 }
---
+++ example-runnable-virtual-threads/src/test/kotlin/org/jesperancinha/vtcc/CoroutinesImplementationTest.kt
@@ -0,0 +1,43 @@
+package org.jesperancinha.vtcc
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+import org.jesperancinha.vtcc.MessageSender.Companion.sendEmail
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.random.Random
+
+
+class MessageSenderCoroutines {
+
+   suspend fun sendMessage(text: String, users: List<User>) = coroutineScope {
+       users.chunked(50).map {
+           async {
+               it.map {
+                   async {
+                       retry(5, 500) {
+                           sendEmail(text, it)
+                       }
+                   }
+               }.awaitAll()
+           }
+       }.awaitAll()
+   }
+}
+
+
+class CoroutinesImplementationTest {
+
+    @Test
+    fun `should test coroutines`(): Unit = runBlocking {
+        val messageSender = MessageSenderCoroutines()
+        messageSender.sendMessage("This is a message", (1..100).map {
+            User(
+                id = Random.nextLong(), name = "user-${UUID.randomUUID()}", email = "user-${UUID.randomUUID()}"
+            )
+        })
+
+    }
+}
