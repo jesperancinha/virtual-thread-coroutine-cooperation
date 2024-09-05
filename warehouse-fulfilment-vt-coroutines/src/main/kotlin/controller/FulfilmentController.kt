199f13bf34c2b3c9e220ea54378aafb0a665fd37
jesperancinha <jofisaes@gmail.com>
2024-09-05 11:32:40 +0200

    Synthetical colaboration between virtual threads and Kotlin coroutines

--- warehouse-fulfilment-vt-coroutines/src/main/kotlin/controller/FulfilmentController.kt
+++ warehouse-fulfilment-vt-coroutines/src/main/kotlin/controller/FulfilmentController.kt
@@ -1,13 +1,17 @@
 package org.jesperancinha.controller
 
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.withContext
 import org.jesperancinha.domain.IsleType.Room
 import org.jesperancinha.domain.Product
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.cglib.proxy.Dispatcher
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.lang.Thread.sleep
+import java.util.concurrent.Executors
 import kotlin.time.Duration.Companion.milliseconds
 
 @RestController
@@ -15,11 +19,15 @@ import kotlin.time.Duration.Companion.milliseconds
 class FulfilmentController {
 
     @GetMapping
-    fun getItems() = (1..10).map {
-//        sleep(10.milliseconds.inWholeMilliseconds)
-        val product = Product(name = "TV", isleType = Room)
-        logger.info("Product: $product")
-        product.name
+    suspend fun getItems() = withContext(
+        Executors.newVirtualThreadPerTaskExecutor()
+            .asCoroutineDispatcher()
+    ) {
+        (1..10).map {
+            val product = Product(name = "TV", isleType = Room)
+            logger.info("Product: $product")
+            product.name
+        }
     }
 
     companion object {
