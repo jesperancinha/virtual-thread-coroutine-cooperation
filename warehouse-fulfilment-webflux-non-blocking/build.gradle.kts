23cc2f4fe1c0b1a06cf7be6ffb28de7c693c522c
Joao Esperancinha <jofisaes@gmail.com>
2024-09-10 08:43:29 +0200

    Adds non-blocking fun to a Flux implementation

---
+++ warehouse-fulfilment-webflux-non-blocking/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+}
+
+configurations.all {
+    exclude(group = "org.slf4j", module = "slf4j-simple")
+}
+
+group = "org.jesperancinha.vtcc"
+version = "0.0.0"
+
+tasks.bootJar {
+    archiveFileName.set("warehouse-fulfilment.jar")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.reactor)
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.register("prepareKotlinBuildScriptModel") {}
--- warehouse-fulfilment-webflux/src/main/kotlin/Main.kt
+++ warehouse-fulfilment-webflux/src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class WebFluxExample
+class WarehouseFulfilmentWebFlux
 
 fun main(args: Array<String>) {
-    SpringApplication.run(WebFluxExample::class.java, *args)
+    SpringApplication.run(WarehouseFulfilmentWebFlux::class.java, *args)
 }
