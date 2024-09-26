8afc53bb3fd6e7ec95c739496d55e7be76f7a7f3
jesperancinha <jofisaes@gmail.com>
2024-09-26 13:17:07 +0200

    Done

--- docker-psql/multiple/data-vtcc.sql
+++ docker-psql/multiple/data-vtcc.sql
@@ -1,6 +1,6 @@
 DO $$
     BEGIN
-        FOR i IN 1..10 LOOP
+        FOR i IN 1..1000 LOOP
                 INSERT INTO product (id, name, isle_type)
                 VALUES (gen_random_uuid(), 'Sample Product ' || i, 'Room');
             END LOOP;
