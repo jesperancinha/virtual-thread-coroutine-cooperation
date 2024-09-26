d2cbc7867d3f0033855cf9c9a8d97cf3511c4512
jesperancinha <jofisaes@gmail.com>
2024-09-26 08:42:27 +0200

    Comments out possible problems

--- warehouse-fulfilment-errors-vt/locust_process.py
+++ warehouse-fulfilment-errors-vt/locust_process.py
@@ -11,13 +11,14 @@ class UserBehavior(TaskSet):
         with self.client.get(url, params=params, headers=headers, catch_response=True) as response:
             if response.status_code == 200:
                 response.success()
-                print(f"Status Code: {response.status_code}, Response: {response.text[:100]}")
+                # print(f"Status Code: {response.status_code}, Response: {response.text[:100]}")
             else:
                 response.failure(f"Failed with status code: {response.status_code}")
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
-    wait_time = between(1, 3)
+    # wait_time = between(1, 3)
+
 
     def on_start(self):
         pass
