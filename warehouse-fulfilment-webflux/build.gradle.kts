6eba6f83e6612af53b7d3fa47677611f26bb65c9
jesperancinha <jofisaes@gmail.com>
2024-09-26 16:57:58 +0200

    Removes postgres library

--- warehouse-fulfilment-webflux/build.gradle.kts
+++ warehouse-fulfilment-webflux/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation(libs.r2dbc.postgresql)
-    implementation(libs.postgresql)
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
