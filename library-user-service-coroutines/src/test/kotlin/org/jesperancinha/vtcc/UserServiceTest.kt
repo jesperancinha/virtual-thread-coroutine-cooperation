42650cd47cf3c6176c268e1f30f0f1a4eee4f6c4
João Esperancinha <jofisaes@gmail.com>
2024-10-03 20:26:59 +0200

    Code for coroutine name

--- library-user-service-coroutines/src/test/kotlin/org/jesperancinha/vtcc/UserServiceTest.kt
+++ library-user-service-coroutines/src/test/kotlin/org/jesperancinha/vtcc/UserServiceTest.kt
@@ -1,6 +1,7 @@
 package org.jesperancinha.vtcc
 
 import io.kotest.matchers.collections.shouldHaveSize
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.toList
@@ -12,9 +13,10 @@ class UserServiceTest {
     private val userService = UserService()
 
     @Test
-    fun `should run main tests`(): Unit = runBlocking(Dispatchers.IO) {
+    fun `should run main tests`(): Unit = runBlocking(Dispatchers.IO + CoroutineName("My special coroutine")) {
         userService.loadUserData(1000)
         delay(2000)
         userService.getAllUsers().toList() shouldHaveSize  1
+        println(this.coroutineContext)
     }
 }
